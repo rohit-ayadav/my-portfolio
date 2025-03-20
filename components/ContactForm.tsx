5ed74c03e3e039ff3347a905b75ce0f315d1e9d8
Rohit kumar yadav <rohitkuyada@gmail.com>
2025-03-21 01:05:01 +0530

    Fix typo in success message of ContactForm component

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -59,7 +59,7 @@ const ContactForm = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
                     </svg>
-                    Message sent successfully! I'll get back to you soon.
+                    Message sent successfully! I&apos;ll get back to you soon.
                 </motion.div>
             )}
 
